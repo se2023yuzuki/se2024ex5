dffc3bdf6c28f2c96aef5ab733959dc39a57d87f
yuzuki <r202304711vl@jindai.jp>
2025-01-19 16:27:33 +0900

    ver3

--- main.c
+++ main.c
@@ -6,5 +6,9 @@ int main() {
   int number = 1;
   printf("The number is: %d\n", number);
 
+  for (int i = 1; i <= 5; i++) {
+    printf("+ %d: The number is %d\n", i, number + i);
+  }
+
   return 0;
 }
